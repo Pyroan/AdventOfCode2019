281b0d69727de7769e4511d51769081a3419e3bf
Violet Moore <Pyroan@users.noreply.github.com>
2019-12-05 23:28:22 -0800

    ENSMALLEN IT

--- day6/day6min.py
+++ day6/day6min.py
@@ -1,5 +1,4 @@
-
-t,l={n[1]:n[0]for n in[s.strip().split(')')for s in open('day6.txt').readlines()]},0
+t,l={n[4:7]:n[:3]for n in open('day6.txt').readlines()},0
 for n in t:
  while n!='COM':l,n=l+1,t[n]
 print(l)
